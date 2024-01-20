ad77d7949063fee4fa79f83c03a75238de66b979
Bohdan Korogodsky <bogdankorogodskij@gmail.com>
2024-01-20 17:49:37 +0300

    +

--- expa.cpp
+++ expa.cpp
@@ -59,27 +59,40 @@ int main()
     std::cout << "unSignLongInt = " << unSignLongInt << std::endl;
 
     long long longLong = 345345345536;
-    
+    std::cout << "longLong = " << longLong << std::endl;
 
     long long int longLongInt = 234234;
+    std::cout << "longLongInt = " << longLongInt << std::endl;
 
     signed long long int signLongLongInt = 234234;
+    std::cout << "signLongLongInt = " << signLongLongInt << std::endl;
     signed long long signLongLong = 1234234;
+    std::cout << "signLongLong = " << signLongLong << std::endl;
 
     unsigned long long unSignLongLong = 234;
+    std::cout << "unSignLongLong = " << unSignLongLong << std::endl;
     unsigned long long int unSignedLongLong = 234234;
+    std::cout << "unSignedLongLong = " << unSignedLongLong << std::endl;
 
     int num = 1'234'567'890;
+    std::cout << "num = " << num << std::endl;
 
     int num1{ 0x1A };
+    std::cout << "num1 = " << num1 << std::endl;
     int num2{ 0xFF };
+    std::cout << "num2 = " << num2 << std::endl;
     int num3{ 0xFFFFFF };
+    std::cout << "num3 = " << num3 << std::endl;
     int vosm1{ 034 };
+    std::cout << "vosm1 = " << vosm1 << std::endl;
     int vosm2{ 0234 };
+    std::cout << "vosm2 = " << vosm2 << std::endl;
     int biNum1{ 0b11010 };
+    std::cout << "biNum1 = " << biNum1 << std::endl;
     int biNum2{ 0b11111111 };
+    std::cout << "biNum2 = " << biNum2 << std::endl;
     std::cout << "biNum2 = " << biNum1 << "\n";
-    // int num2
+    
 
     std::cout << "Hello World!\n";
     std::cout << "privet\n";
